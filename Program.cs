b1efb5f8116c61e51b0f377c425d19f432727087
Gabriel D'Amico <gabrieldamico223@outlook.com>
2024-10-06 05:24:54 +0200

    DriverPage changes test - added dropdown list

--- Program.cs
+++ Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
